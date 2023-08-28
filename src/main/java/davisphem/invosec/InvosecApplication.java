357a9c946ce62329ad2af9aa34c783e742cb5dc7
David Oluwafemi <davidoluwafemi178@gmail.com>
2023-08-28 22:01:19 +0100

    first commit

---
+++ src/main/java/davisphem/invosec/InvosecApplication.java
@@ -0,0 +1,12 @@
+package davisphem.invosec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvosecApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InvosecApplication.class, args);
+  }
+}
---
+++ src/test/java/davisphem/invosec/InvosecApplicationTests.java
@@ -0,0 +1,13 @@
+package davisphem.invosec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvosecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
